60088e3aab1b8c731b80dc6a2965948754b3838b
tebogokgopa <tebogokgopa84@gmail.com>
2023-06-19 15:25:20 +0200

    bash update

--- github.sh
+++ github.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
-echo "github script to automate repository creation and update"
 
-if ["$1" -eq push];
-then
-    git status;
-else
-    echo "No argument entered script halts"
-fi
+# Variables
+GITHUB_USERNAME="your_github_username"
+GITHUB_TOKEN="your_github_token"
+REPO_OWNER="repository_owner"
+REPO_NAME="repository_name"
+BRANCH_NAME="branch_name"
+REMOTE_NAME="origin"
+TARGET_DIRECTORY="/path/to/target_directory"
+
+# Clone the repository
+git clone "https://github.com/$REPO_OWNER/$REPO_NAME.git" "$TARGET_DIRECTORY"
+cd "$TARGET_DIRECTORY"
+
+# Set up Git configuration
+git config user.name "$GITHUB_USERNAME"
+git config user.email "$GITHUB_USERNAME@users.noreply.github.com"
+
+# Checkout the desired branch
+git checkout "$BRANCH_NAME"
+
+# Make changes to your code or build your project
+
+# Commit changes
+git add .
+git commit -m "Automated CI/CD commit"
+
+# Push changes to the remote repository
+git remote set-url "$REMOTE_NAME" "https://$GITHUB_USERNAME:$GITHUB_TOKEN@github.com/$REPO_OWNER/$REPO_NAME.git"
+git push "$REMOTE_NAME" "$BRANCH_NAME"
